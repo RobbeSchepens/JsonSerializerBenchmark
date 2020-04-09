0c450a17c3795b5d299b5d36f5876c46a7897c51
Robbe Schepens <robbe.schepens@spray.com>
2020-04-09 15:01:28 +0200

    Add project files.

---
+++ JsonSerializerBenchmark/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using MessagePack;
+using Newtonsoft.Json;
+using ServiceStack;
+
+namespace JsonSerializerBenchmark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SerializationBenchmark>();
+            Console.ReadKey();
+        }
+    }
+
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    [MemoryDiagnoser]
+    [MinColumn, MaxColumn]
+    public class SerializationBenchmark
+    {
+        [Benchmark]
+        public void SerializeAndDeserializeInNewtonSoft()
+        {
+            var userProfile = CreateNewUserProfile();
+
+            for (var i = 0; i < 1_000_000; i++)
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(userProfile);
+                var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<UserProfile>(json);
+            }
+        }
+
+        //[Benchmark]
+        //public void SerializeAndDeserializeInMessagePack()
+        //{
+        //    MessagePackSerializer.SetDefaultResolver(MessagePack.Resolvers.ContractlessStandardResolver.Instance);
+        //    var userProfile = CreateNewUserProfile();
+
+        //    for (var i = 0; i < 1_000_000; i++)
+        //    {
+        //        var json = MessagePackSerializer.SerializeToJson(userProfile);
+        //        var obj = MessagePackSerializer.ConvertFromJson(json);
+        //    }
+        //}
+
+        [Benchmark]
+        public void SerializeAndDeserializeInServiceStackText()
+        {
+            var userProfile = CreateNewUserProfile();
+
+            for (var i = 0; i < 1_000_000; i++)
+            {
+                var json = userProfile.ToJson();
+                var obj = json.FromJson<UserProfile>();
+            }
+        }
+
+        [Benchmark]
+        public void SerializeAndDeserializeInJil()
+        {
+            var userProfile = CreateNewUserProfile();
+
+            for (var i = 0; i < 1_000_000; i++)
+            {
+                var json = Jil.JSON.Serialize(userProfile);
+                var obj = Jil.JSON.Deserialize<UserProfile>(json);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SerializeAndDeserializeInTextJson()
+        {
+            var userProfile = CreateNewUserProfile();
+
+            for (var i = 0; i < 1_000_000; i++)
+            {
+                var json = System.Text.Json.JsonSerializer.Serialize(userProfile);
+                var obj = System.Text.Json.JsonSerializer.Deserialize<UserProfile>(json);
+            }
+        }
+
+        [Benchmark]
+        public void SerializeAndDeserializeInUtf8Json()
+        {
+            var userProfile = CreateNewUserProfile();
+
+            for (var i = 0; i < 1_000_000; i++)
+            {
+                var json = Utf8Json.JsonSerializer.Serialize(userProfile);
+                var obj = Utf8Json.JsonSerializer.Deserialize<UserProfile>(json);
+            }
+        }
+
+        private UserProfile CreateNewUserProfile()
+        {
+            return new UserProfile
+            {
+                ID = 1234,
+                NetworkID = "1234",
+                Domain = "WHTN",
+                CICSOPID = "WTS",
+                CCN = "WHTN",
+                EmployeeNbr = "1234",
+                LastName = "Robbe",
+                FirstName = "Robbe",
+                MiddleInit = "",
+                MfgLocationID = "",
+                LanguageCode = "",
+                TimeZone = "",
+                DivMDTCode = "",
+                DateFormat = "sdddddddd",
+                ActiveCD = "",
+                AMAPSOPID = "",
+                AMAPSPWD = "",
+                PrimaryRepDeptNo = "",
+                Email = "sqQDSQSDQDSQSDDQS",
+                NativeLastName = "",
+                NativeFirstName = "",
+                MgrEmpCode = "",
+                ShortLangCode = "",
+                SecondaryRepNos = "",
+                ViewCommissions = true,
+                DeptNos = "sddddddddd",
+                ViewCostData = true,
+                ViewIntraFacility = false,
+                PurchasingSpentLimit = "",
+                lastUpdateDate = DateTime.Now,
+                firstUpdateDate = DateTime.UtcNow,
+                IntraFacilityRepNos = "",
+                GMTID = 9,
+                ViewGrossMargin = true,
+                AssignedSalesmanCDs = "",
+                SSCoEmp = "",
+                AzureObjectId = new Guid("F0E999CA-05F0-4DF2-9DE7-1D533B82EC0B"),
+                UserPrincipalName = "",
+                dhGuid = new Guid("F0E999CA-05F0-4DF2-9DE7-1D533B82EC0B"),
+                Test = new Test(),
+            };
+        }
+    }
+}
---
+++ JsonSerializerBenchmark/UserProfile.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonSerializerBenchmark
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+    }
+
+	public class UserProfile
+    {
+		public int ID { get; set; }
+
+		public string NetworkID { get; set; }
+
+		public string Domain { get; set; }
+
+		public string CICSOPID { get; set; }
+
+		public string CCN { get; set; }
+
+		//[JsonIgnore]
+		public string EmployeeNbr { get; set; }
+
+		public string LastName { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string MiddleInit { get; set; }
+
+		public string MfgLocationID { get; set; }
+
+		public string LanguageCode { get; set; }
+
+		public string TimeZone { get; set; }
+
+		public string DivMDTCode { get; set; }
+
+		public string DateFormat { get; set; }
+
+		public string ActiveCD { get; set; }
+
+		public string AMAPSOPID { get; set; }
+
+		public string AMAPSPWD { get; set; }
+
+		public string GMTTime { get; set; }
+
+		public string PrimarySalesmanCD { get; set; }
+
+		public string RepSecurity { get; set; }
+
+		public string PaidCommisions { get; set; }
+
+		public string RepDeptCode { get; set; }
+
+		public string PrimaryRepDeptNo { get; set; }
+
+		public string Email { get; set; }
+
+		public string NativeLastName { get; set; }
+
+		public string NativeFirstName { get; set; }
+
+		public string MgrEmpCode { get; set; }
+
+		public string ShortLangCode { get; set; }
+
+		public string SecondaryRepNos { get; set; }
+
+		public bool ViewCommissions { get; set; }
+
+		public string DeptNos { get; set; }
+
+		public bool? ViewCustData { get; set; }
+
+		public bool? ViewCostData { get; set; }
+
+		public bool? ViewIntraFacility { get; set; }
+
+		public string PurchasingSpentLimit { get; set; }
+
+		public DateTime? lastUpdateDate { get; set; }
+
+		public DateTime? firstUpdateDate { get; set; }
+
+		public int? lastUpdateUser { get; set; }
+
+		public string IntraFacilityRepNos { get; set; }
+
+		public int? GMTID { get; set; }
+
+		public bool ViewGrossMargin { get; set; }
+
+		public string AssignedSalesmanCDs { get; set; }
+
+		public string SSCoEmp { get; set; }
+
+		public int? PurLimitid { get; set; }
+
+		public Guid? AzureObjectId { get; set; }
+
+		public string UserPrincipalName { get; set; }
+
+		public Guid dhGuid { get; set; }
+
+		public Test Test { get; set; }
+	}
+}
